d5e542ecaf4f3535125d07ac49905f84d71552c7
Simon <keller.simon1998@web.de>
2022-11-06 01:31:58 +0100

    integrated mongodb client library

--- ServerSource/src/main.cpp
+++ ServerSource/src/main.cpp
@@ -13,6 +13,8 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include <mongoc/mongoc.h>
+
 
 std::string make_daytime_string()
 {
@@ -23,6 +25,79 @@ std::string make_daytime_string()
 
 int main()
 {
+    const char* uri_string = "mongodb://localhost:27017";
+    mongoc_client_t* client = nullptr;
+    mongoc_database_t* database = nullptr;
+    mongoc_collection_t* collection = nullptr;
+    bson_t* command, reply, * insert;
+    bson_error_t error;
+    char* str;
+    bool retval;
+
+    /*
+     * Required to initialize libmongoc's internals
+     */
+    mongoc_init();
+
+    /*
+     * Create a new client instance
+     */
+    client = mongoc_client_new("mongodb://simon:Lt2bQb8jpRaLSn@185.242.113.159/?authSource=networkdata");
+    if (!client) {
+        return EXIT_FAILURE;
+    }
+
+    /*
+     * Register the application name so we can track it in the profile logs
+     * on the server. This can also be done from the URI (see other examples).
+     */
+    mongoc_client_set_appname(client, "DataCollectorServer");
+
+    /*
+     * Get a handle on the database "db_name" and collection "coll_name"
+     */
+    database = mongoc_client_get_database(client, "networkdata");
+    collection = mongoc_client_get_collection(client, "networkdata", "test");
+
+    /*
+     * Do work. This example pings the database, prints the result as JSON and
+     * performs an insert
+     */
+    command = BCON_NEW("ping", BCON_INT32(1));
+
+    retval = mongoc_client_command_simple(
+        client, "admin", command, NULL, &reply, &error);
+
+    if (!retval) {
+        fprintf(stderr, "%s\n", error.message);
+        return EXIT_FAILURE;
+    }
+
+    str = bson_as_json(&reply, NULL);
+    printf("%s\n", str);
+
+    insert = BCON_NEW("hello", BCON_UTF8("world"));
+
+    if (!mongoc_collection_insert_one(collection, insert, NULL, NULL, &error)) {
+        fprintf(stderr, "%s\n", error.message);
+    }
+
+    bson_destroy(insert);
+    bson_destroy(&reply);
+    bson_destroy(command);
+    bson_free(str);
+
+    /*
+     * Release our handles and clean up libmongoc
+     */
+    mongoc_collection_destroy(collection);
+    mongoc_database_destroy(database);
+    mongoc_client_destroy(client);
+    mongoc_cleanup();
+
+    return EXIT_SUCCESS;
+
+
 
 	using asio_tcp = asio::ip::tcp;
 	std::cout << "start server" << std::endl;
