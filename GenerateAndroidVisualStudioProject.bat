d18526462f4d0cb25aa445822060a5fc2ba308c3
Simon <Simon@DESKTOP-2TSEAF9>
2023-12-01 00:44:00 +0100

    changed app name to NAT Collector

--- GenerateAndroidVisualStudioProject.bat
+++ GenerateAndroidVisualStudioProject.bat
@@ -6,4 +6,4 @@ cmake -G "Visual Studio 17 2022" -A ARM -B bin/Android ^
 	-DCMAKE_ANDROID_STL_TYPE=c++_static 
 
 devenv bin\Android\AndroidNativeActivity.sln ^
-	/Command "File.AddExistingProject %cd%\AndroidPackaging\AndroidPackaging.androidproj" 
+	/Command "File.AddExistingProject %cd%\AndroidPackaging\NAT.androidproj" 
