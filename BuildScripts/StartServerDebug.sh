0f66adba5b5629ce658ee4b4efb4c335bd78fa62
Simon Keller <simon@vmanager7706.premium-vserver.net>
2024-06-28 01:55:49 +0200

    added debug config

---
+++ BuildScripts/StartServerDebug.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd ..
+if [ -d "unix_build" ]; then 
+	echo "Delete last build."
+	rm -Rf "unix_build";
+fi
+
+mkdir -p "unix_build"
+cd "unix_build"
+
+# Run CMake in parent folder
+cmake ..
+make
+
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+    # If the build was successful, execute the binary
+    sudo systemctl restart nat-server-debug.service
+    sudo systemctl status nat-server-debug.service
+    cat /var/log/nat-server-debug.log
+else
+    # If the build failed, display an error message
+    echo "Build failed. Please check the CMake configuration and fix any errors."
+fi
