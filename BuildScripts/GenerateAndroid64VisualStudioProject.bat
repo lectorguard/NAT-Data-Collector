41f6bd66d82e99770aca502da91323157818b658
Simon <keller.simon1998@web.de>
2025-02-11 20:07:15 +0100

    updated debug settings

--- BuildScripts/GenerateAndroid64VisualStudioProject.bat
+++ BuildScripts/GenerateAndroid64VisualStudioProject.bat
@@ -1,9 +1,9 @@
 cmake -G "Visual Studio 17 2022" -A ARM64 -B ../bin/Android ^
 	-DCMAKE_SYSTEM_NAME=Android ^
-	-DCMAKE_SYSTEM_VERSION=24	^
+	-DCMAKE_SYSTEM_VERSION=29 ^
 	-DCMAKE_ANDROID_ARCH_ABI=arm64-v8a  ^
 	-DANDROID_ABI=arm64-v8a ^
-	-DCMAKE_ANDROID_NDK=C:/Microsoft/AndroidNDK/android-ndk-r23c ^
+	-DCMAKE_ANDROID_NDK="C:/Program Files (x86)/Android/android-sdk/ndk-bundle/" ^
 	-DCMAKE_ANDROID_STL_TYPE=c++_static ^
 	"-DCMAKE_VS_GLOBALS=UseMultiToolTask=true;EnforceProcessCountAcrossBuilds=true" ^
 	..
